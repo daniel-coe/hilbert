970a5991abf99db2aedf424f7d73a6f69403d461
Daniel Coe <daniel.coe@christancay.services>
2019-03-11 22:18:32 -0400

    Generate Rainbow Hilbert curve; break out functions for OOP

---
+++ FileHead.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PixelHilbert
+{
+    static class FileHead
+    {
+        public static void Read(this byte[] data, string path)
+        {
+            using (FileStream fStream = File.Open(@"C:\Users\Daniel Coe\Pictures\" + path, FileMode.Open))
+            {
+                for (int i = 0; i < data.Length; i++)
+                {
+                    data[i] = (byte)fStream.ReadByte();
+                }
+            }
+        }
+        public static void WriteImage(this Pixel[,] pixels, byte[] header, string path)
+        {
+            using (FileStream fStream = File.Open(@"C:\Users\Daniel Coe\Pictures\" + path, FileMode.Create))
+            {
+                byte[] file = new byte[54 + 3 * pixels.Length];
+                for (int i = 0; i < file.Length; i++)
+                {
+                    if (i < 54)
+                    {
+                        file[i] = header[i];
+                    }
+                    else
+                    {
+                        Pixel p = pixels[(i - 54) / 96, (i / 3 - 18) % 32];
+                        switch (i % 3)
+                        {
+                            case 0:
+                                file[i] = p.b;
+                                break;
+                            case 1:
+                                file[i] = p.g;
+                                break;
+                            case 2:
+                                file[i] = p.r;
+                                break;
+                        }
+                    }
+                }
+                fStream.Write(file, 0, file.Length);
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using static System.Math;
 
 namespace PixelHilbert
 {
@@ -13,19 +14,20 @@ namespace PixelHilbert
         {
             Random rand = new Random();
             Point coord = new Point { x = 0, y = 0 };
-            List<int> direx = null;
+            List<int> direx = new List<int>();
             int i = 0;
             byte red = 255; byte green = 0; byte blue = 0;
             Pixel[,] image = new Pixel[32, 32];
             byte[] header = new byte[54];
-            Func<Point, int, Point> move = (p, n) => n % 2 == 0 ? new Point { x = p.x + n == 0 ? 1 : -1, y = p.y } : new Point { x = p.x, y = p.y + n == 1 ? 1 : -1 };
+            Func<Point, int, Point> move = (p, n) => n % 2 == 0 ? new Point { x = p.x + (n == 0 ? 1 : -1), y = p.y } : new Point { x = p.x, y = p.y + (n == 1 ? 1 : -1) };
             while (i < 1024)
             {
-                image[coord.x, coord.y] = new Pixel { r = red, g = green, b = blue };
-                if (i==direx.Capacity)
+                image[coord.y, coord.x] = new Pixel { r = red, g = green, b = blue };
+                if (i==direx.Count)
                 {
-
+                    IterateHilbert(ref direx);
                 }
+                Console.WriteLine(direx[i]);
                 switch (i / 255 % 6)
                 {
                     case 0:
@@ -47,43 +49,46 @@ namespace PixelHilbert
                         blue--;
                         break;
                 }
+                coord = move(coord, direx[i]);
                 i++;
             }
-            using (FileStream fStream = File.Open(@"C:\Users\Daniel Coe\Pictures\1024.bmp", FileMode.Open))
+            image = SphericGreyscale(image);
+            header.Read("1024.bmp");
+            image.WriteImage(header, "gen1.bmp");
+        }
+        static void IterateHilbert(ref List<int> ds)
+        {
+            for (int i = 0; i < 2; i++)
             {
-                for (int j = 0; j < header.Length; j++)
+                var temp = ds.ToArray<int>();
+                ds.Add((1 - i) % 4);
+                foreach (var d in temp)
                 {
-                    header[j] = (byte)fStream.ReadByte();
+                    ds.Add((5 - d) % 4);
+                }
+                ds.Add(i % 4);
+                foreach (var d in temp)
+                {
+                    ds.Add((5 - d) % 4);
+                }
+                ds.Add((3 - i) % 4);
+                foreach (var d in temp)
+                {
+                    ds.Add((d + 2) % 4);
                 }
             }
-            using (FileStream fStream = File.Open(@"C:\Users\Daniel Coe\Pictures\gen0.bmp", FileMode.Create))
+        }
+        static Pixel[,] SphericGreyscale(Pixel[,] img)
+        {
+            for (int i = 0; i < img.GetLength(0); i++)
             {
-                byte[] file = new byte[header.Length + 3 * i];
-                for (int j = 0; j < file.Length; j++)
+                for (int j = 0; j < img.GetLength(1); j++)
                 {
-                    if (j < 54)
-                    {
-                        file[j] = header[j];
-                    }
-                    else
-                    {
-                        Pixel p = image[(j - 54) / 96, (j / 3 - 18) % 32];
-                        switch (j % 3)
-                        {
-                            case 0:
-                                file[j] = p.b;
-                                break;
-                            case 1:
-                                file[j] = p.g;
-                                break;
-                            case 2:
-                                file[j] = p.r;
-                                break;
-                        }
-                    }
+                    byte v = (byte)Sqrt((Pow(img[i, j].r, 2) + Pow(img[i, j].g, 2) + Pow(img[i, j].b, 2)) / 3);
+                    img[i, j] = new Pixel { r = v, g = v, b = v };
                 }
-                fStream.Write(file, 0, file.Length);
             }
+            return img;
         }
     }
     struct Pixel
